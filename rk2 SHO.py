a039d369ca6cc186b6e8db121119fdbacfcd01e2
Mahatapa Chakraborty <113203112+MahatapaChakraborty@users.noreply.github.com>
2024-12-20 19:55:09 +0530

    Add files via upload
    done of pyroid

--- rk2 SHO.py
+++ rk2 SHO.py
@@ -55,6 +55,7 @@ def euler(time,x0,z0):
 
 def rk2(time,euler,eulervel,x0,z0):
 	rk2=[]
+	rk2vel=[]
 	xrk2old=x0
 	velrk2old=z0
 	
@@ -62,17 +63,36 @@ def rk2(time,euler,eulervel,x0,z0):
 		velrk2new=velrk2old-(h/2)*(xrk2old+euler[i])
 		xrk2new=xrk2old+(h/2)*(velrk2old+eulervel[i])
 		rk2.append(xrk2new)
+		rk2vel.append(velrk2new)
 		xrk2old=xrk2new
 		velrk2old=velrk2new
 		
 	return(rk2)
 	
+def rk2vel(time,euler,eulervel,x0,z0):
+	rk2=[]
+	rk2vel=[]
+	xrk2old=x0
+	velrk2old=z0
+	
+	for i in range(len(time)):
+		velrk2new=velrk2old-(h/2)*(xrk2old+euler[i])
+		xrk2new=xrk2old+(h/2)*(velrk2old+eulervel[i])
+		rk2.append(xrk2new)
+		rk2vel.append(velrk2new)
+		xrk2old=xrk2new
+		velrk2old=velrk2new
+		
+	return(rk2vel)
+	
 plt.xlabel("time")
 plt.ylabel("position")	
-plt.plot(time,rk2(time,euler(time,0,1),eulervel(time,0,1),0,1),color="b",label="rk2 soln")
-plt.plot(time,euler(time,0,1),color="r",label="eulers soln")
-plt.plot(time,np.sin(time),color="g",label="original sol")
-plt.title('simple harmonic osscilator using rk2 intial pos 0 intial vel 1 h=0.0125')
+plt.plot(time,rk2(time,euler(time,0,1),eulervel(time,0,1),0,1),color="b",label="rk2 solnn")
+#plt.plot(time,rk2vel(time,euler(time,0,1),eulervel(time,0,1),0,1),color='k',label='rk2 velocity')
+plt.plot(time,euler(time,0,1),color="r",label="euler soln")
+#plt.plot(time,eulervel(time,0,1),color='g',label='euler velocity')
+plt.plot(time,np.sin(time),color="g",label="original soln")
+plt.title('simple harmonic osscilator result using rk2 intial pos 0 intial vel 1 h=0.0125')
 plt.grid()
 plt.legend()
 
