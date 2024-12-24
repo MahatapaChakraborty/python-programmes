64712d8f66f8d013b1eaa1d4c79e4eaacda3b3c6
Mahatapa Chakraborty <113203112+MahatapaChakraborty@users.noreply.github.com>
2024-12-24 19:42:12 +0530

    Add files via upload

---
+++ driven harmonic osscilator/driven harmonic osscilator.py
@@ -0,0 +1,85 @@
+#driven harmonic osscilator
+#d2x/dt2 + 2*a*dx/dt + (w0^2)*x =f0*cos(wd*t)
+#original sol. x=f0/(((wd^2-w0^2)^2 + 4*a^2*wd^2)^0.5) cos(wd*t-phi)<-particular soln.
+#               tan(phi)=2*a*wd/(wd^2-w0^2)
+#resonat freq(max amp)=wd^2=wR^2=w0^2-2*(a^2)
+#dz/dt=(f0*cos(wd*t))-(w0^2)*x-2*a*z and dx/dt=z
+#z(n+1)=z(n)+h*(f0*cos(wd*tn)-(w0^2)*xn-2*a*zn) and x(n+1)=x(n)+h*zn 
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+time=[]
+t0=0#initial time
+tn=10#final time
+n=10000#total no. of points-1
+h=(tn-t0)/n
+for i in range(n+1):
+    t=t0+i*h
+    time.append(t)
+
+#print(time)
+#initial conditions
+x0=2#<-initial position
+z0=0#<-initial velocity
+#w0=10#<-natural frequency
+#wd=20#<-driven frequency
+a=4#<-damping coeff/m
+f0=50
+
+def driven(a,z,w0,x,f0,wd,t):
+    p1=f0*(np.cos(wd*t))
+    p2=(w0**2)*x
+    p3=2*a*z
+    return(p1-p2-p3)
+
+def xsoleuler(time,x0,z0,a,w0,f0,wd):
+    xsoleuler=[]
+    veuler=[]
+    xinitial=x0
+    vinitial=z0
+    xold=xinitial
+    vold=vinitial
+    for i in range(len(time)):
+        xnew=xold+h*vold
+        vnew=vold+h*driven(a,vold,w0,xold,f0,wd,time[i])
+        veuler.append(vnew)
+        xsoleuler.append(xnew)
+        xold=xnew
+        vold=vnew
+
+    return(xsoleuler)
+
+#print(xsoleuler(time,x0,z0,a,w0,f0,wd))
+
+def veleuler(time,x0,z0,a,w0,f0,wd):
+    xsoleuler=[]
+    veuler=[]
+    xinitial=x0
+    vinitial=z0
+    xold=xinitial
+    vold=vinitial
+    for i in range(len(time)):
+        xnew=xold+h*vold
+        vnew=vold+h*driven(a,vold,w0,xold,f0,wd,time[i])
+        veuler.append(vnew)
+        xsoleuler.append(xnew)
+        xold=xnew
+        vold=vnew
+
+    return(veuler)
+
+plt.xlabel('time(t)')
+plt.ylabel('position(x)')
+plt.plot(time,xsoleuler(time,x0,z0,a,10,f0,4),color='b',label='wd=4')
+#plt.plot(time,xsoleuler(time,x0,z0,a,10,f0,10),color='g',label='wd=10')
+plt.plot(time,xsoleuler(time,x0,z0,a,10,f0,8.24),color='r',label='wd=8.24 resonant freq')
+plt.plot(time,xsoleuler(time,x0,z0,a,10,f0,13),color='k',label='wd=13')
+plt.title('d2x/dt2+2*a*dx/dt+(w0^2)*x=f0*cos(wd*t),x0=2,z0=0,a=4,f0=50,w0=10,h=0.001')
+plt.legend()
+plt.grid()
+plt.show()
+        
+        
+
+    
