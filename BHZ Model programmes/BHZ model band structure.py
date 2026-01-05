ab6e7a24f415e6a8f53dd6f2a4c4df7584d07238
Mahatapa Chakraborty <113203112+MahatapaChakraborty@users.noreply.github.com>
2026-01-06 00:47:57 +0530

    Add files via upload

--- BHZ Model programmes/BHZ model band structure.py
+++ BHZ Model programmes/BHZ model band structure.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 N=200
-m=2.5
-kxpoints=np.linspace(-2*np.pi,2*np.pi,N)
-kypoints=np.linspace(-2*np.pi,2*np.pi,N)
+m=0
+kxpoints=np.linspace(0,2*np.pi,N)
+kypoints=np.linspace(0,2*np.pi,N)
 
 Kx,Ky=np.meshgrid(kxpoints,kypoints)
 
@@ -16,18 +16,23 @@ def eigenvalueH(kx,ky,m):
     H22=-H11
 
     H=np.array([[H11, H12],[H21, H22]])
-    return(np.linalg.eigvals(H))
+    E=np.linalg.eigvals(H)
+    Ereal=[]
+    for i in range(len(E)):
+        Ereal.append(E[i].real)
+    Ereal.sort()
+    return(Ereal)
 
 #print(eigenvalueH(3,3,2))
 greatereigval=np.zeros((N,N), dtype=complex)
 for i in range(N):
     for j in range(N):
-        greatereigval[i,j]=eigenvalueH(kxpoints[i],kypoints[j],m)[0]
+        greatereigval[i,j]=eigenvalueH(kxpoints[i],kypoints[j],m)[1]
 
 lessereigval=np.zeros((N,N), dtype=complex)
 for i in range(N):
     for j in range(N):
-        lessereigval[i,j]=eigenvalueH(kxpoints[i],kypoints[j],m)[1]
+        lessereigval[i,j]=eigenvalueH(kxpoints[i],kypoints[j],m)[0]
         
 #fig=plt.figure(figsize=(10,7))
 #ax=fig.add_subplot(111,projection='3d')
@@ -38,6 +43,7 @@ for i in range(N):
 #ax.set_xlabel('Kx')
 #ax.set_ylabel('Ky')
 #ax.set_zlabel('Ek')
+#ax.set_title('Band structure 2X2 BHZ model m=5')
 
 #plt.show()
 plt.figure(figsize=(10,7))
@@ -45,5 +51,5 @@ plt.pcolormesh(Kx,Ky,np.real(greatereigval-lessereigval),shading='auto',cmap='co
 plt.colorbar(label="Difference between two bands")
 plt.xlabel("k_x values")
 plt.ylabel("k_y values")
-plt.title("m=2.5 difference between bands")
+plt.title("m=0 difference between bands BHZ 2X2 model")
 plt.show()
